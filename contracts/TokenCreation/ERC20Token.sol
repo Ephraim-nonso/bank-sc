15a0a921393af8043d99c0b9a524216e6da9effe
Chukwu Ephraim <Chukwuchinonsoephraim@gmail.com>
2022-02-15 23:39:51 +0100

    CHORE: creation of ERC20 token

---
+++ contracts/TokenCreation/ERC20Token.sol
@@ -0,0 +1,139 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/4a9cc8b4918ef3736229a5cc5a310bdc17bf759f/contracts/utils/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+contract ERC20Token is IERC20Metadata {
+
+    using SafeMath for uint;
+
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+    uint256 private _totalSupply;
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowed;
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_, uint256 totalSupply_) {
+        _name = name_;
+        _symbol = symbol_;
+        _decimals = decimals_;
+        _totalSupply = totalSupply_ ** decimals_;
+        _balances[msg.sender] = _totalSupply;
+    }
+
+     /**
+     * @dev Returns the name of the token.
+     */
+    function name() external override view returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Returns the symbol of the token.
+     */
+    function symbol() external override view returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns the decimals places of the token.
+     */
+    function decimals() external view returns (uint8) {
+        return _decimals;
+    }
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external override view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256){
+        return _balances[account];
+    }
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool) {
+        require(amount <= _balances[msg.sender], "Insufficient funds");
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        _balances[to] = _balances[to].add(amount);
+        return true;
+        emit(msg.sender, to, amount);
+    }
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256) {
+        return _allowed[owner][spender];
+    }
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool) {
+        
+    }
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool) {
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        _allowed[msg.sender][from] = _allowed[msg.sender][].add(amount);
+        return true;
+    }
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    // event Transfer(address indexed from, address indexed to, uint256 value);
+
+    // /**
+    //  * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+    //  * a call to {approve}. `value` is the new allowance.
+    //  */
+    // event Approval(address indexed owner, address indexed spender, uint256 value);
+
+}
