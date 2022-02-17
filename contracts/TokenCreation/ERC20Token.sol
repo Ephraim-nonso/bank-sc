1d08302bce2c573e9c0969b17948f26b17eedf64
Chukwu Ephraim <Chukwuchinonsoephraim@gmail.com>
2022-02-17 13:12:49 +0100

    CHORE: removal of redundant comments

--- contracts/TokenCreation/ERC20Token.sol
+++ contracts/TokenCreation/ERC20Token.sol
@@ -138,21 +138,5 @@ contract ERC20Token is IERC20Metadata {
         _transfer(from, to, amount);
         emit Transfer(msg.sender, to, amount);
         return true;
-        
     }
-
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
-    // event Transfer(address indexed from, address indexed to, uint256 value);
-
-    // /**
-    //  * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-    //  * a call to {approve}. `value` is the new allowance.
-    //  */
-    // event Approval(address indexed owner, address indexed spender, uint256 value);
-
 }
