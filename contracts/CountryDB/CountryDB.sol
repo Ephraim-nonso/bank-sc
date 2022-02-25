38c75633f258908fc3f8f8d71c33817a52a95ad2
Chukwu Ephraim <Chukwuchinonsoephraim@gmail.com>
2022-02-25 23:39:54 +0100

    DB: more feat

--- contracts/CountryDB/CountryDB.sol
+++ contracts/CountryDB/CountryDB.sol
@@ -11,13 +11,22 @@ contract CountryDB {
     }
 
     struct LGA {
+        string lgaName;
         string Chairman;
         uint Revenue;
         string[] markets;
         mapping(string => Village) villages;
     }
 
+    struct LGAWithoutMapping {
+        string lgaName;
+        string Chairman;
+        uint Revenue;
+        string[] markets;
+    }
+
     struct State {
+        string stateName;
         string governor;
         string capital;
         uint[] zipCodes;
@@ -26,20 +35,19 @@ contract CountryDB {
     }
 
     struct StateWithoutmapping {
+        string stateName;
         string governor;
         string capital;
         uint[] zipCodes;
         uint IGR;
     }
 
-    struct CountryWithoutStates {
-        string president;
-        string capital;
-        uint GDP;
-        uint countryRevenue;
-    }
-
+    /*@Dev
+    *This is a struct of the Country
+    *with a mapping.
+    */
     struct Country {
+        string countryName;
         string president;
         string capital;
         uint GDP;
@@ -49,20 +57,37 @@ contract CountryDB {
 
     mapping(string => Country) countries;
 
+    /*@Dev
+    *This is a struct of the Country
+    *without a mapping. To aid in the display
+    *of the country.
+    */
+    struct CountryWithoutStates {
+        string countryName;
+        string president;
+        string capital;
+        uint GDP;
+        uint countryRevenue;
+    }
+
+    /*@dev
+    The three function adds new country, state
+    and a LGA
+    */
+
     function addCountry(
         string calldata _cn, 
         string calldata _p,
         string calldata _c,
         uint _gdp,
-        uint _cR,
-        string memory addstate
+        uint _cR
         ) external {
         Country storage c = countries[_cn];
+        c.countryName = _cn;
         c.president = _p;
         c.capital = _c;
         c.GDP = _gdp;
         c.countryRevenue = _cR;
-        c.states[addstate];
     }
 
     function addState(
@@ -74,32 +99,32 @@ contract CountryDB {
         uint _IGR
     ) external {
         State storage state = countries[_cn].states[_state];
-        // State storage state = country.states[_state];
+        state.stateName = _state;
         state.governor = _governor;
         state.capital = _capital;
         state.zipCodes = _zipCodes;
         state.IGR = _IGR;
     }
 
-    // function addLGA(
-    //     string calldata _lga;
-    //     string calldata _cn,
-    //       string calldata _governor;
-    //     string calldata _capital;
-    //     uint[] memory _zipCodes;
-    //     uint _IGR;
-    // ) external {
-    //     Country storage country = contries[_cn];
-    //     State storage state = country.states[_lga];
-    //     state.governor = _governor;
-    //     state.capital = _capital;
-    //     state.zipCodes = _zipCodes;
-    //     state.IGR = _IGR;
-    // }
+    function addLGA(
+        string calldata _cn,
+        string calldata _st,
+        string calldata _lg,
+        string calldata _chairman,
+        uint _Revenue,
+        string[] memory _markets
+    ) external {
+       LGA storage lga = countries[_cn].states[_st].LGAs[_lg];
+        lga.lgaName = _lg;
+        lga.Chairman = _chairman;
+        lga.Revenue= _Revenue;
+        lga.markets = _markets;
+    }
 
     //see a single country.
     function seeCountry(string calldata _cn) external view returns(CountryWithoutStates memory c) {
         Country storage singleCountry = countries[_cn];
+        c.countryName = singleCountry.countryName;
         c.president = singleCountry.president;
         c.capital = singleCountry.capital;
         c.GDP = singleCountry.GDP;
@@ -109,10 +134,22 @@ contract CountryDB {
     //view a single state
     function seeState(string calldata _st, string calldata _c) external view returns (StateWithoutmapping memory s) {
         State storage state = countries[_c].states[_st];
+        s.stateName = state.stateName;
         s.governor = state.governor;
         s.capital = state.capital;
         s.zipCodes = state.zipCodes;
         s.IGR = state.IGR;
+    }
+
+    function seeLGA(string calldata _lg,
+    string calldata _st,
+    string calldata _cn
+    ) view public returns (LGAWithoutMapping memory lg) {
+        LGA storage lgReplica = countries[_cn].states[_st].LGAs[_lg];
+        lg.lgaName = lgReplica.lgaName;
+        lg.Chairman = lgReplica.Chairman;
+        lg.Revenue = lgReplica.Revenue;
+        lg.markets = lgReplica.markets;
     }
       
 }
